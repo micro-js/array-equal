c861808559360ccca738ec97c4fba4979ed12e8b
Andrew Shaffer <darawk@gmail.com>
2015-12-06 12:12:58 -0800

    update package to new style

--- test/index.js
+++ test/index.js
@@ -3,17 +3,16 @@
  */
 
 var arrayEqual = require('..')
-var assert = require('assert')
+var test = require('tape')
 
 /**
  * Tests
  */
 
-describe('arrayEqual', function () {
-  it('should work', function () {
-    assert.equal(arrayEqual([1, 2], [1, 2]), true)
-    assert.equal(arrayEqual([1, 2], [1, 2, 3]), false)
-    assert.equal(arrayEqual([], [1]), false)
-    assert.equal(arrayEqual([1], [2]), false)
-  })
+test('should work', function (t) {
+  t.equal(arrayEqual([1, 2], [1, 2]), true)
+  t.equal(arrayEqual([1, 2], [1, 2, 3]), false)
+  t.equal(arrayEqual([], [1]), false)
+  t.equal(arrayEqual([1], [2]), false)
+  t.end()
 })
