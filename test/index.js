672a37b463e7e227446b8ad904de907ed9c1a1a4
Josh Taylor <joshrtay@gmail.com>
2015-12-16 11:14:56 -0800

    update exports

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * Imports
  */
 
-var arrayEqual = require('..')
+var equal = require('..')
 var test = require('tape')
 
 /**
@@ -10,9 +10,9 @@ var test = require('tape')
  */
 
 test('should work', function (t) {
-  t.equal(arrayEqual([1, 2], [1, 2]), true)
-  t.equal(arrayEqual([1, 2], [1, 2, 3]), false)
-  t.equal(arrayEqual([], [1]), false)
-  t.equal(arrayEqual([1], [2]), false)
+  t.equal(equal([1, 2], [1, 2]), true)
+  t.equal(equal([1, 2], [1, 2, 3]), false)
+  t.equal(equal([], [1]), false)
+  t.equal(equal([1], [2]), false)
   t.end()
 })
